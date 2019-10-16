909da18a627e61dd022690f9a83282cb5b644cc9
Simon O'Donnell <simonrodonnell@yahoo.co.uk>
2019-10-16 18:59:24 +0100

    pangram tests passed

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,11 @@
 const IsogramFinder = function (word) {
-
+  this.wordArray = word.toLowerCase().split('');
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
+  return this.wordArray.every((letter, index) => {
+    return this.wordArray.indexOf(letter) === index;
+  })
 }
 
 module.exports = IsogramFinder;

d1b2a75befb777496aad521b500db806605051e2
Simon O'Donnell <simonrodonnell@yahoo.co.uk>
2019-10-16 18:40:26 +0100

    pangram test passed

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,12 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase.toLowerCase().replace(/\s+/g, '').split('');
 }
 
 PangramFinder.prototype.isPangram = function () {
-
+  return this.alphabet.every((letter) => {
+    return this.phrase.includes(letter);
+  })
 }
 
 module.exports = PangramFinder;

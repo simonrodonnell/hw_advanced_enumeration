6f99717988f99561313bd7c2b2d4cc15a2659376
Simon O'Donnell <simonrodonnell@yahoo.co.uk>
2019-10-16 20:11:26 +0100

    anagram test passed

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,9 +1,26 @@
 const AnagramFinder = function (word) {
-
+  this.comparisonWord = word;
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
 
-}
+  const prepWord = function(wordToSort){
+    const wordToArray = wordToSort.toLowerCase().split('').sort();
+    const wordToString = wordToArray.toString().replace(/,+/g, '')
+    return wordToString;
+  }
+
+  const comparisonWordSorted = prepWord(this.comparisonWord);
+
+  return otherWords.filter((word) => {
+    if (word === this.comparisonWord) {
+      return false;
+    } else
+    { const otherWordSorted = prepWord(word);
+      return otherWordSorted === comparisonWordSorted;
+    }
+  })
+};
+
 
 module.exports = AnagramFinder;
